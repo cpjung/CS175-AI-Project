420f0c7fac058eec4424825c9514d2a9089260c1
Wing <vwing@ics.uci.edu>
2016-02-08 14:28:40 -0800

    Tried out nltk stuff. Need python 2 to make it work

--- parseRawCSV.py
+++ parseRawCSV.py
@@ -2,8 +2,14 @@ import csv
 import pandas as pd
 
 quests = pd.read_csv("quests.csv")
-print(list(quests.columns.values))
+#print(list(quests.columns.values))
 details = quests.Details
 rawStr = ""
 for d in details:
     rawStr += str(d)
+
+import nltk
+nltk.download('punkt')
+tokens = nltk.word_tokenize(rawStr)
+text = nltk.Text(tokens)
+print(text.generate(3))
