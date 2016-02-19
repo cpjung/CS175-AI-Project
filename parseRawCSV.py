d2a6443cd9a5d3b0247a6ce589170f1f1008268a
Vwing <vwing@uci.edu>
2016-02-19 15:23:43 -0800

    Added format_simple method to remove punctuations

--- parseRawCSV.py
+++ parseRawCSV.py
@@ -1,4 +1,5 @@
 import csv
+import re
 import pandas as pd
 
 quests = pd.read_csv("quests.csv")
@@ -12,12 +13,21 @@ import nltk
 
 def generate_model(cfdist, word, num=15):
     for i in range(num):
+        #print(word + " ")
         print word + " "
         word = cfdist[word].max()
+        
+def format_simple(document):
+    doc = document.lower()                                           #convert to lower case
+    doc = re.sub(r'[\.\?\{\}\[\]\\\|\(\)!,:\'-;\"]','',doc)         #remove punctuations
+    doc = re.sub(r'[\s]+',' ', doc)                                  #remove whitespace clumps
+    doc =  doc.strip()                                               #remove trailing whitespace
+    return doc
 
+rawStr = format_simple(rawStr)
 tokens = nltk.word_tokenize(rawStr)
 text = nltk.Text(tokens)
 bigrams = nltk.bigrams(text)
 cfd = nltk.ConditionalFreqDist(bigrams)
 
-generate_model(cfd, 'King')
+generate_model(cfd, 'king')
