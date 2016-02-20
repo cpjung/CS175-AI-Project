8ed0b76fb130448135d4a9f5846ef32ab484a424
Vwing <vwing@uci.edu>
2016-02-19 20:52:16 -0800

    Added get_ran_word function

--- parseRawCSV.py
+++ parseRawCSV.py
@@ -1,6 +1,8 @@
 import csv
 import re
 import pandas as pd
+import random
+import copy
 
 quests = pd.read_csv("quests.csv")
 #print(list(quests.columns.values))
@@ -11,15 +13,23 @@ for d in details:
 
 import nltk
 
+def get_ran_word(cfdist,word,sampleNum):
+    d = copy.copy(cfdist[word])
+    words = [None] * sampleNum
+    for i in range(sampleNum):
+        words[i] = d.max();
+        del(d[words[i]]);
+    return random.choice(words)
+
 def generate_model(cfdist, word, num=15):
     for i in range(num):
-        #print(word + " ")
-        print word + " "
-        word = cfdist[word].max()
+        print(word + " ")
+        #print word + " "
+        word = get_ran_word(cfdist,word,3)
         
 def format_simple(document):
     doc = document.lower()                                           #convert to lower case
-    doc = re.sub(r'[\.\?\{\}\[\]\\\|\(\)!,:\'-;\"]','',doc)         #remove punctuations
+    doc = re.sub(r'[\.\?\{\}\[\]\\\|\(\)!,:\'-;\$\"]','',doc)         #remove punctuations
     doc = re.sub(r'[\s]+',' ', doc)                                  #remove whitespace clumps
     doc =  doc.strip()                                               #remove trailing whitespace
     return doc
@@ -30,4 +40,4 @@ text = nltk.Text(tokens)
 bigrams = nltk.bigrams(text)
 cfd = nltk.ConditionalFreqDist(bigrams)
 
-generate_model(cfd, 'king')
+generate_model(cfd, 'collect')
